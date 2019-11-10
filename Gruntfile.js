544bfbd8d7c91cf28ff184cf912e863e410433a1
Shinya Hijiri <shinya.hijiri@gmail.com>
2019-11-11 02:48:17 +0900

    GruntでKarmaを実行できるタスク作成

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,20 @@
 module.exports = function(grunt) {
 
+    grunt.initConfig({
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+
     // A very basic default task.
     grunt.registerTask('default', 'Log some stuff.', function() {
         grunt.log.write('Logging some stuff...').ok();
     });
+
+    grunt.registerTask('test', ['karma']);
 
 };
